dc899ef0482d159806dbb8ba0f9d5c989272925b
xavialex <xavialex7@gmail.com>
2017-06-01 18:11:32 +0200

    Preprocesado sólo con pandas en limpio

--- Polynomial Regression.py
+++ Polynomial Regression.py
@@ -21,18 +21,7 @@ continuous_features = ['LotFrontage', 'LotArea', 'MasVnrArea', 'BsmtFinSF1', 'Bs
                        'ScreenPorch', 'PoolArea', 'MiscVal']
 
 # Filling null values
-"""
-from sklearn.preprocessing import Imputer
-imp_mean = Imputer(missing_values="NaN", strategy='mean', axis = 0)
-imp_most_frequent = Imputer(missing_values="NaN", strategy='most_frequent', axis = 0)
-imp_median = Imputer(missing_values="NaN", strategy='median', axis = 0)
-"""
-
-X.loc[:, 'LotFrontage'] = X.loc[:, 'LotFrontage'].fillna(X.loc[:, 'LotFrontage'].mean())
-X.loc[:, 'BsmtFinSF1'] = X.loc[:, 'BsmtFinSF1'].fillna(X.loc[:, 'BsmtFinSF1'].mean())
-X.loc[:, 'BsmtFinSF2'] = X.loc[:, 'BsmtFinSF2'].fillna(X.loc[:, 'BsmtFinSF2'].mean())
 X[continuous_features] = X[continuous_features].fillna(X[continuous_features].mean())
-a = X[continuous_features].fillna(X[continuous_features].mean())
 
 X.loc[:, 'Alley'] = X.loc[:, 'Alley'].fillna(X.loc[:, 'Alley'].value_counts().idxmax()) # Lots of NaN's, consider removing
 X.loc[:, 'Electrical'] = X.loc[:, 'Electrical'].fillna(X.loc[:, 'Electrical'].value_counts().idxmax())
@@ -69,63 +58,6 @@ X[continuous_features] = StandardScaler().fit_transform(X[continuous_features])
 # Splitting into training examples and test examples
 X_train = X.iloc[:1460, :]
 X_test = X.iloc[1460:, :]
-
-"""
-# Encoding categorical data in X
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-X.loc[:, 'MSSubClass'] = LabelEncoder().fit_transform(X.loc[:, 'MSSubClass'])
-X.loc[:, 'MSZoning'] = LabelEncoder().fit_transform(X.loc[:, 'MSZoning'])
-X.loc[:, 'Street'] = LabelEncoder().fit_transform(X.loc[:, 'Street'])
-X.loc[:, 'Alley'] = LabelEncoder().fit_transform(X.loc[:, 'Alley'])
-X.loc[:, 'LotShape'] = LabelEncoder().fit_transform(X.loc[:, 'LotShape'])
-X.loc[:, 'LandContour'] = LabelEncoder().fit_transform(X.loc[:, 'LandContour'])
-X.loc[:, 'Utilities'] = LabelEncoder().fit_transform(X.loc[:, 'Utilities'])
-X.loc[:, 'LotConfig'] = LabelEncoder().fit_transform(X.loc[:, 'LotConfig'])
-X.loc[:, 'LandSlope'] = LabelEncoder().fit_transform(X.loc[:, 'LandSlope'])
-X.loc[:, 'Neighborhood'] = LabelEncoder().fit_transform(X.loc[:, 'Neighborhood'])
-X.loc[:, 'Condition1'] = LabelEncoder().fit_transform(X.loc[:, 'Condition1'])
-X.loc[:, 'Condition2'] = LabelEncoder().fit_transform(X.loc[:, 'Condition2'])
-X.loc[:, 'BldgType'] = LabelEncoder().fit_transform(X.loc[:, 'BldgType'])
-X.loc[:, 'HouseStyle'] = LabelEncoder().fit_transform(X.loc[:, 'HouseStyle'])
-X.loc[:, 'OverallQual'] = LabelEncoder().fit_transform(X.loc[:, 'OverallQual'])
-X.loc[:, 'OverallCond'] = LabelEncoder().fit_transform(X.loc[:, 'OverallCond'])
-X.loc[:, 'YearBuilt'] = LabelEncoder().fit_transform(X.loc[:, 'YearBuilt'])
-X.loc[:, 'YearRemodAdd'] = LabelEncoder().fit_transform(X.loc[:, 'YearRemodAdd'])
-X.loc[:, 'RoofStyle'] = LabelEncoder().fit_transform(X.loc[:, 'RoofStyle'])
-X.loc[:, 'RoofMatl'] = LabelEncoder().fit_transform(X.loc[:, 'RoofMatl'])
-X.loc[:, 'Exterior1st'] = LabelEncoder().fit_transform(X.loc[:, 'Exterior1st'])
-X.loc[:, 'Exterior2nd'] = LabelEncoder().fit_transform(X.loc[:, 'Exterior2nd'])
-X.loc[:, 'MasVnrType'] = LabelEncoder().fit_transform(X.loc[:, 'MasVnrType'])
-X.loc[:, 'ExterQual'] = LabelEncoder().fit_transform(X.loc[:, 'ExterQual'])
-X.loc[:, 'ExterCond'] = LabelEncoder().fit_transform(X.loc[:, 'ExterCond'])
-X.loc[:, 'Foundation'] = LabelEncoder().fit_transform(X.loc[:, 'Foundation'])
-X.loc[:, 'BsmtQual'] = LabelEncoder().fit_transform(X.loc[:, 'BsmtQual'])
-X.loc[:, 'BsmtCond'] = LabelEncoder().fit_transform(X.loc[:, 'BsmtCond'])
-X.loc[:, 'BsmtExposure'] = LabelEncoder().fit_transform(X.loc[:, 'BsmtExposure'])
-X.loc[:, 'BsmtFinType1'] = LabelEncoder().fit_transform(X.loc[:, 'BsmtFinType1'])
-X.loc[:, 'BsmtFinType2'] = LabelEncoder().fit_transform(X.loc[:, 'BsmtFinType2'])
-X.loc[:, 'Heating'] = LabelEncoder().fit_transform(X.loc[:, 'Heating'])
-X.loc[:, 'Heating'] = LabelEncoder().fit_transform(X.loc[:, 'MSSubClass'])
-X.loc[:, 'HeatingQC'] = LabelEncoder().fit_transform(X.loc[:, 'HeatingQC'])
-X.loc[:, 'CentralAir'] = LabelEncoder().fit_transform(X.loc[:, 'CentralAir'])
-X.loc[:, 'Electrical'] = LabelEncoder().fit_transform(X.loc[:, 'Electrical'])
-X.loc[:, 'KitchenQual'] = LabelEncoder().fit_transform(X.loc[:, 'KitchenQual'])
-X.loc[:, 'Functional'] = LabelEncoder().fit_transform(X.loc[:, 'Functional'])
-X.loc[:, 'FireplaceQu'] = LabelEncoder().fit_transform(X.loc[:, 'FireplaceQu'])
-X.loc[:, 'GarageType'] = LabelEncoder().fit_transform(X.loc[:, 'GarageType'])
-X.loc[:, 'GarageYrBlt'] = LabelEncoder().fit_transform(X.loc[:, 'GarageYrBlt'])
-X.loc[:, 'GarageFinish'] = LabelEncoder().fit_transform(X.loc[:, 'GarageFinish'])
-X.loc[:, 'GarageQual'] = LabelEncoder().fit_transform(X.loc[:, 'GarageQual'])
-X.loc[:, 'GarageCond'] = LabelEncoder().fit_transform(X.loc[:, 'GarageCond'])
-X.loc[:, 'PavedDrive'] = LabelEncoder().fit_transform(X.loc[:, 'PavedDrive'])
-X.loc[:, 'PoolQC'] = LabelEncoder().fit_transform(X.loc[:, 'PoolQC'])
-X.loc[:, 'Fence'] = LabelEncoder().fit_transform(X.loc[:, 'Fence'])
-X.loc[:, 'MiscFeature'] = LabelEncoder().fit_transform(X.loc[:, 'MiscFeature'])
-X.loc[:, 'MoSold'] = LabelEncoder().fit_transform(X.loc[:, 'MoSold'])
-X.loc[:, 'YrSold'] = LabelEncoder().fit_transform(X.loc[:, 'YrSold'])
-X.loc[:, 'SaleType'] = LabelEncoder().fit_transform(X.loc[:, 'SaleType'])
-X.loc[:, 'SaleCondition'] = LabelEncoder().fit_transform(X.loc[:, 'SaleCondition'])
-"""
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
