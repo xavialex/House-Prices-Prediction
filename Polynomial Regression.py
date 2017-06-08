f06be7d8b10e28bafa259965bc7960cee560574b
xavialex <xavialex7@gmail.com>
2017-06-08 18:22:31 +0200

    Mejoras e intentos

--- Polynomial Regression.py
+++ Polynomial Regression.py
@@ -13,12 +13,29 @@ X_train = training_set.iloc[:, :-1]
 y = training_set.iloc[:, -1].values
 X_test = pd.read_csv('Data/test.csv')
 X = X_train.append(X_test)
+X.update(X.drop('Id', axis=1, inplace=True))
 
 continuous_features = ['LotFrontage', 'LotArea', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 
                        'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 
                        'LowQualFinSF', 'GrLivArea', 'GarageArea', 'WoodDeckSF', 
                        'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 
                        'ScreenPorch', 'PoolArea', 'MiscVal']
+
+categorical_features = ['MSSubClass', 'MSZoning', 'Street', 'Alley', 'LotShape', 
+                        'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 
+                        'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 
+                        'HouseStyle', 'OverallQual', 'OverallCond', 'YearBuilt', 
+                        'YearRemodAdd', 'RoofStyle', 'RoofMatl', 'Exterior1st', 
+                        'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 
+                        'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 
+                        'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 
+                        'CentralAir', 'Electrical', 'BsmtFullBath', 'BsmtHalfBath', 
+                        'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 
+                        'KitchenQual', 'TotRmsAbvGrd', 'Functional', 'Fireplaces', 
+                        'FireplaceQu', 'GarageType', 'GarageYrBlt', 'GarageFinish', 
+                        'GarageCars', 'GarageQual', 'GarageCond', 'PavedDrive', 
+                        'PoolQC', 'Fence', 'MiscFeature', 'MoSold', 'YrSold', 
+                        'SaleType', 'SaleCondition']
 
 # Filling null values
 X[continuous_features] = X[continuous_features].fillna(X[continuous_features].mean())
@@ -29,6 +46,9 @@ X.loc[:, 'Electrical'] = X.loc[:, 'Electrical'].fillna(X.loc[:, 'Electrical'].va
 X = X.fillna('i')
 X.loc[:, 'GarageYrBlt'] = X.loc[:, 'GarageYrBlt'].replace('i', 0)
 X.loc[:, 'MasVnrArea'] = X.loc[:, 'MasVnrArea'].replace('i', 0)
+
+X = pd.get_dummies(X, drop_first=True, columns=categorical_features)
+
 
 X = pd.get_dummies(X, drop_first=True,
                            columns=['MSSubClass', 'MSZoning', 'Street', 'Alley',
@@ -74,8 +94,24 @@ svr_rbf = SVR(kernel='rbf')
 print(datetime.datetime.now())
 y_poly1 = svr_poly.fit(X_train1, y_train1).predict(X_train1)
 y_poly2 = svr_poly.fit(X_train1, y_train1).predict(X_train2)
-y_rbf1 = svr_rbf.fit(X_train1.loc[:, 'LotArea'].to_frame(), y_train1).predict(X_train1.loc[:, 'LotArea'].to_frame())
+
+y_rbf2 = svr_rbf.fit(X_train1[:, 5].reshape(-1, 1), y_train1).predict(X_train2[:, 2].reshape(-1, 1))
+
+y_rbf1 = svr_rbf.fit(X_train1.loc[:, 'GarageArea'].to_frame(), y_train1).predict(X_train1.loc[:, 'GarageArea'].to_frame())
+y_rbf1 = svr_rbf.fit(X_train1.loc[:, 'LotFrontage'].to_frame(), y_train1).predict(X_train1.loc[:, 'LotFrontage'].to_frame())
 y_rbf2 = svr_rbf.fit(X_train1, y_train1).predict(X_train2)
+
+""" 
+SVR siempre devuelve 163000, ¿qué pasa?
+"""
+
+# Fitting Decision Tree Regressor
+from sklearn import tree
+clf = tree.DecisionTreeRegressor()
+clf = clf.fit(X_train1, y_train1)
+a = clf.predict(X_train1)
+b = clf.predict(X_train2)
+
 
 # Fitting Linear Regression to the dataset
 from sklearn.linear_model import LinearRegression
@@ -87,13 +123,38 @@ y_pred2 = lin_reg.predict(X_train2)
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 2) # Muy mal comportamiento
-X_poly = poly_reg.fit_transform(X_train1)
-X_poly2 = poly_reg.fit_transform(X_train2)
-poly_reg.fit(X_poly, y_train1)
+#0 Idea, expandir polinómicamente sólo las vbles continuas, en proceso, ¿cómo le agrego luego las discretas?
+X_poly = poly_reg.fit_transform(X.loc[:, continuous_features])
+X_poly = X.loc[:, categorical_features].join(pd.DataFrame(X_poly))
+X_poly = pd.get_dummies(X_poly, drop_first=True, columns=categorical_features)
+X_poly_train = X_poly[:1460]
+X_poly_test = X_poly[1460:]
+X_poly1, X_poly2, y_train1, y_train2 = train_test_split(X_poly_train, y, test_size = 0.2, random_state = 0)
+poly_reg.fit(X_poly1, y_train1)
 lin_reg = LinearRegression()
-lin_reg.fit(X_poly, y_train1)
-y_pred1 = lin_reg.predict(X_poly)
+lin_reg.fit(X_poly1, y_train1)
+y_pred1 = lin_reg.predict(X_poly1)
 y_pred2 = lin_reg.predict(X_poly2)
+
+plt.scatter(np.arange(0, 2919), y_pred1)
+plt.scatter(np.arange(0, 1168), y_train1, edgecolors='red')
+plt.ylim(0, 500000)
+plt.show()
+
+# Fitting XGBoost to the Training set
+import os
+
+mingw_path = 'C:\\Program Files\\mingw-w64\\x86_64-5.3.0-posix-seh-rt_v4-rev0\\mingw64\\bin'
+
+os.environ['PATH'] = mingw_path + ';' + os.environ['PATH']
+
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train1, y_train1) # Nunca acaba de entrenarse
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_train1)
+y_pred2 = classifier.predict(X_train2)
 
 
 # Evaluating the model
@@ -103,21 +164,19 @@ R_sq2 = r2_score(y_train2, y_rbf2)
 R_sq1 = r2_score(y_train1, y_pred1)
 R_sq2 = r2_score(y_train2, y_pred2)
 
-# Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
 """ Diario de resultados:
     La regresión lineal es el algoritmo que mejor funciona: R^2 para test de 0.3865
     Ni SVR con kernel rbf ni regresiones polinómicas se ajustan siquiera a los ejemplos de entrenamiento
     ¿Qué va mal?
+    ---
+    Decision Tree Regressor: R^2 de 0.719. No está mal, pero mucho margen de mejora
 """
 
 # Visualising the Polynomial Regression results
+plt.scatter(training_set.loc[:, 'LotFrontage'], y)
+plt.scatter(training_set.loc[:, 'LotArea'], y)
+
+
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
